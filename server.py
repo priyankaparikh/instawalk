786e26533c62cf35874f60bfe575f0d80df10bdc
vagrant <vagrant@vagrant.vm>
2018-05-18 20:35:43 +0000

    added register and login routes

--- server.py
+++ server.py
@@ -2,15 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def landing_page():
     """Render landing page."""
 
     return render_template('landing.html')
 
+
 @app.route('/register')
 def register_user():
-    """Add a user to the database"""
+    """Add a user to the database."""
+
+
+@app.route('/login')
+def login_user():
+    """Login an already registered user."""
+
 
 
 if __name__ == "__main__":
