7820ed5afdcc266f1a376e22a0c7354415fa8a98
vagrant <vagrant@vagrant.vm>
2018-05-18 20:16:55 +0000

    started server with a langing.html page

--- server.py
+++ server.py
@@ -1,1 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def landing_page():
+    """Render landing page."""
+
+    return render_template('landing.html')
+
+
+if __name__ == "__main__":
+
+    app.run(port=5000, host='0.0.0.0', debug=True)
