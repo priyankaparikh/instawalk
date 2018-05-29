bf925b25a8faae5d54411d4d3ceeeb9b76d1f9ea
Jessie Engstrom <jessielengstrom@gmail.com>
2018-05-29 16:15:08 -0700

    database

--- models.py
+++ models.py
@@ -262,7 +262,7 @@ def connect_to_db(app, db_uri=DATABASE_URL):
 
     # Configure to use our PstgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ECHO'] = True
     db.app = app
     db.init_app(app)
