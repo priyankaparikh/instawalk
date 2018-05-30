ee720d26e32190eac4bf9896ee94c7c3d2cf8216
Jessie Engstrom <jessielengstrom@gmail.com>
2018-05-30 13:21:57 -0700

    deploy

--- server.py
+++ server.py
@@ -6,17 +6,17 @@ import os
 
 app = Flask(__name__)
 app.secret_key = 'ABCD'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xllajhyyvxmohg:586b67a7d8124b2dfbe064491e95b5c80e5042511be61108c393956923d3302a@ec2-54-225-107-174.compute-1.amazonaws.com:5432/d82mvtff7vnuge'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xllajhyyvxmohg:586b67a7d8124b2dfbe064491e95b5c80e5042511be61108c393956923d3302a@ec2-54-225-107-174.compute-1.amazonaws.com:5432/d82mvtff7vnuge'
 app.config['UPLOAD_FOLDER'] = './static/user_sourced_photos'
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-from models import connect_to_db, db
+# from models import connect_to_db, db
 from models import (User, Comp_Routes, User_Routes, Route, Waypoint, Step, Path,
                     Direction)
 from sqlalchemy import func
 import queries
 
-import queries
+# import queries
 
 # app.jinja_env.undefined = StrictUndefined
 
@@ -355,5 +355,5 @@ if __name__ == "__main__":
     # app.debug = True
     # app.jinja_env.auto_reload = app.debug
 
-    connect_to_db(app)
-    app.run(port=5000, host='0.0.0.0', debug=True)
+    # connect_to_db(app)
+    app.run()
